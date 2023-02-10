a5584a32d2afd0e75797dc9235876c94c85b6a7c
Anton Vishnevski <anton@procyon.ee>
2023-02-10 18:32:15 +0200

    Solution

--- src/Sheep.java
+++ src/Sheep.java
@@ -1,14 +1,33 @@
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 public class Sheep {
 
    enum Animal {sheep, goat};
    
    public static void main (String[] param) {
-      // for debugging
+
    }
    
    public static void reorder (Animal[] animals) {
-      // TODO!!! Your program here
+      int n_goats = 0;
+      int n_sheeps = 0;
+      for (int x=0; x < animals.length; x++) {
+         if (animals[x] == Animal.goat) {
+            n_goats++;
+         } else {
+            n_sheeps++;
+         }
+      }
+      int y = 0;
+      while (y < n_goats) {
+         animals[y] = Animal.goat;
+         y++;
+      }
+      while (y < (n_sheeps + n_goats)) {
+         animals[y] = Animal.sheep;
+         y++;
+      }
    }
 }
 
